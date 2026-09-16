47847c21e29fb1de4f065594faa4bb233afc2619
Houl Chansothearith <houlchansothearith@gmail.com>
2026-09-16 13:16:01 +0700

    update

---
+++ notepad-frontend/src/stores/notefilter.ts
@@ -0,0 +1,23 @@
+import { defineStore } from "pinia";
+import { computed, ref } from "vue";
+
+export const useNoteFilterStore = defineStore("filterNote", () => {
+  const currFilter = ref(1);
+
+  const filterKey = computed<string | null>(() => {
+    switch (currFilter.value) {
+      case 1:
+        return "isPinned";
+      case 2:
+        return "isDeleted";
+      default:
+        return null;
+    }
+  });
+
+  function setFilter(filter: number) {
+    currFilter.value = filter;
+  }
+
+  return { currFilter, filterKey, setFilter };
+});
